9c8a55262740bac419527a8c2c5ace5a8f1635f9
ZaenbA <honeysorataiga@gmail.com>
2025-08-07 18:02:50 +0100

    Remove exposed credentials and add .gitignore

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault('SECRET_KEY', 'django-insecure-c+-0r$)e0wjfw!i=)k^f5m#wd@3txr%8=_*pcy*%#^x9=t&3c%')
-
-os.environ.setdefault("DATABASE_URL","postgresql://neondb_owner:npg_LSrHqyA8EGh0@ep-shiny-firefly-a21z8d5n.eu-central-1.aws.neon.tech/pry_lair_koala_177059")
