5060ba2ea9b900868687bbb1a8ba1359c24d6f36
ZaenbA <honeysorataiga@gmail.com>
2025-08-11 14:14:28 +0100

    add admin register model  in admin.py

--- moods/admin.py
+++ moods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MoodEntry
 
 # Register your models here.
+@admin.register(MoodEntry)
+class MoodEntryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'mood', 'intensity', 'created_at')
+    list_filter = ('user', 'mood', 'created_at')
+    search_fields = ('user__username', 'mood__name',)
